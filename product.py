5765820057af42119836dcbea4ae9ca0a1d48284
resteve <resteve@zikzakmedia.com>
2013-10-02 19:49:58 +0200

    pyflakes

--- product.py
+++ product.py
@@ -2,7 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import ModelSQL, fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 from trytond.pyson import Eval
 from .tools import slugify
 
--- tools.py
+++ tools.py
@@ -1,9 +1,7 @@
 #This file is part product_esale module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-
 import logging
-import time
 
 try:
     import slug
