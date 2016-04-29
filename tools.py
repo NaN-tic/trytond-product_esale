0186cfa7a77f776b38a8d0cb1ba605403c1630e2
resteve <resteve@zikzakmedia.com>
2016-04-29 13:07:09 +0200

    Add template engines (genshi, jinja2 and simple eval) in tools

--- tools.py
+++ tools.py
@@ -1,13 +1,13 @@
 # This file is part product_esale module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-import logging
+import slug
+from simpleeval import simple_eval
+from genshi.template import NewTextTemplate as TextTemplate
+from jinja2 import Template as Jinja2Template
+from trytond.config import config as config_
 
-try:
-    import slug
-except ImportError:
-    logger = logging.getLogger(__name__)
-    logger.error('Unable to import slug. Install slug package.')
+template_engine = config_.get('product', 'template_engine', default='genshi')
 
 def slugify(value):
     """Convert value to slug: az09 and replace spaces by -"""
@@ -25,3 +25,52 @@ def seo_lenght(string):
     if len(string) > 155:
         return '%s...' % (string[:152])
     return string
+
+def esale_eval(expression, record):
+    '''Evaluates the given :attr:expression
+
+    :param expression: Expression to evaluate
+    :param record: The browse record of the record
+    '''
+    if template_engine == 'genshi':
+        return _engine_genshi(expression, record)
+    elif template_engine == 'jinja2':
+        return _engine_jinja2(expression, record)
+    else:
+        return _engine_python(expression, record)
+
+def template_context(record):
+    """ Generate the template context
+    This is mainly to assist in the inheritance pattern
+    """
+    return {'record': record}
+
+def _engine_python(expression, record):
+    '''Evaluate the pythonic expression and return its value
+    '''
+    if expression is None:
+        return u''
+    tpl_context = template_context(record)
+    return simple_eval(expression, tpl_context)
+
+def _engine_genshi(expression, record):
+    '''
+    :param expression: Expression to evaluate
+    :param record: Browse record
+    '''
+    if not expression:
+        return u''
+    template = TextTemplate(expression)
+    tpl_context = template_context(record)
+    return template.generate(**tpl_context).render(encoding='UTF-8')
+
+def _engine_jinja2(expression, record):
+    '''
+    :param expression: Expression to evaluate
+    :param record: Browse record
+    '''
+    if not expression:
+        return u''
+    template = Jinja2Template(expression)
+    tpl_context = template_context(record)
+    return template.render(tpl_context).encode('utf-8')
