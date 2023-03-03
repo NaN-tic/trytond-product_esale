452409e83c72b77a9326e3d6975a98b30dcbed74
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2023-03-03 13:44:11 +0100

    FIX tests: - Delete unuses module "product_manufacturer". - FIX extra_depends tests - Delete unused views
    Task #048598

--- tests/test_module.py
+++ tests/test_module.py
@@ -9,8 +9,7 @@ from trytond.tests.test_tryton import ModuleTestCase
 class ProductEsaleTestCase(CompanyTestMixin, ModuleTestCase):
     'Test ProductEsale module'
     module = 'product_esale'
-    extras = ['product_manufacturer', 'product_review',
-        'product_template_attribute']
+    extras = ['product_review', 'product_template_attribute', 'purchase']
 
 
 del ModuleTestCase
