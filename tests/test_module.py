2cbfb1d7a0c128487b54101d2ae214cd9a10b119
Albert Cervera i Areny <albert@nan-tic.com>
2026-06-01 14:27:42 +0200

    Remove obsolet product_review module from extras_depend.

--- tests/test_module.py
+++ tests/test_module.py
@@ -9,7 +9,7 @@ from trytond.tests.test_tryton import ModuleTestCase
 class ProductEsaleTestCase(CompanyTestMixin, ModuleTestCase):
     'Test ProductEsale module'
     module = 'product_esale'
-    extras = ['product_review', 'product_template_attribute', 'purchase']
+    extras = ['product_template_attribute', 'purchase']
 
 
 del ModuleTestCase
